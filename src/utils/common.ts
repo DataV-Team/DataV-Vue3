d4b1716a0ffa5ebbf183d3a487729288e62b19d0
wzx <834529118@qq.com>
2022-12-07 11:37:12 +0800

    refactor: Modify the decoration-5 function name

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -2,7 +2,7 @@
  * @Description: 请输入当前文件描述
  * @Author: @Xin (834529118@qq.com)
  * @Date: 2022-12-06 11:33:03
- * @LastEditTime: 2022-12-06 17:23:38
+ * @LastEditTime: 2022-12-07 11:34:58
  * @LastEditors: @Xin (834529118@qq.com)
  */
 import type { App, Plugin } from 'vue';
@@ -19,6 +19,18 @@ export function withInstall<T>(comp: T) {
 }
 
 export const definePropType = <T>(val: any): PropType<T> => val;
+
+/**
+ * @description: 计算两点之间距离
+ * @param {Number[]} pointOne point1
+ * @param {Number[]} pointTwo point2
+ * @return {Number}
+ */
+export const calcTwoPointDistance = (pointOne: number[], pointTwo: number[]) => {
+  var minusX = Math.abs(pointOne[0] - pointTwo[0]);
+  var minusY = Math.abs(pointOne[1] - pointTwo[1]);
+  return Math.sqrt(Math.pow(minusX, 2) + Math.pow(minusY, 2));
+};
 
 export function mergeColor(defaultColors: string[], colors: string[] = []) {
   return merge(defaultColors, colors);
